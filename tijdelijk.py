55bf1a89be92752b292dabc9d279f22852571a36
Sijmon Oevering <OEVE386101@exactsoftware.com>
2024-06-05 15:40:16 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,19 @@
+prijzen = {
+    "Aardbei": 3,
+    "Vanille": 4,
+    "Chocolade": 5
+}
+
+aanbieding = prijzen["Aardbei"] * 0.8
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding:f}"
+
+reclame_tekst2 = reclame_tekst[:-4]
+
+reclame_tekst3 = reclame_tekst2.upper()
+
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el.lower())
